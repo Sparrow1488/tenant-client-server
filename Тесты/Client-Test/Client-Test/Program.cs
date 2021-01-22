890acb9b04b5776fda93b5167ad2098b94a77919
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-22 22:35:52 +0300

    create[Тесты]: начал тестировать работу сервера и клиента в консоли

---
+++ Тесты/Client-Test/Client-Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Test
+{
+    class Program
+    {
+        static TcpClient client = new TcpClient();
+        static IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("App start");
+            client.Connect(serverEndPoint);
+            NetworkStream stream = client.GetStream();
+
+            string request = "Client is connected!";
+            byte[] data = Encoding.UTF8.GetBytes(request);
+            await stream.WriteAsync(data, 0, data.Length);
+            Console.WriteLine("Write in " +  stream.WriteTimeout);
+
+            stream.Close();
+            client.Close();
+            Console.WriteLine("До свзяи...");
+
+        }
+    }
+}
---
+++ Тесты/Client-Test/Server-Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Test
+{
+    public class Program
+    {
+        static IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        static TcpListener server = new TcpListener(localEndPoint);
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("App started");
+
+            server.Start();
+            Console.WriteLine("Server started");
+            
+
+            while (true)
+            {
+                Console.WriteLine("Wait for connecting...");
+                TcpClient getClient = server.AcceptTcpClient();
+                Console.WriteLine("Client connecting");
+
+                NetworkStream stream = getClient.GetStream();
+                byte[] getData = new byte[1024];
+            }
+        }
+    }
+}

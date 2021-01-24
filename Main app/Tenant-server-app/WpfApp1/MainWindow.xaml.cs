c516379ecdadaf34b29d9f2bb3b5174186ae30a1
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-24 16:31:11 +0300

    work[client]: сделан минимальный дизайн клиента

--- Main app/Tenant-server-app/WpfApp1/MainWindow.xaml.cs
+++ Main app/Tenant-server-app/WpfApp1/MainWindow.xaml.cs
@@ -15,34 +15,9 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-
-        private static IFirebaseConfig config = new FirebaseConfig
-        {
-            AuthSecret = "6CScUkKUdSLgSDtq1QWtfY2NCPP57aa6ajBn7R4Y",
-            BasePath = "https://client-server-testapp-default-rtdb.firebaseio.com/"
-        };
-        private static FirebaseClient client = null;
-        private static string parentPath = "Users";
-        private static MyUser ActiveUser = null;
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            client = new FirebaseClient(config);
-            if (client != null)
-            {
-                Console.WriteLine("Client is connected!");
-            }
-            else
-            {
-                throw new ArgumentNullException("Ошибка подключения");
-            }
-        }
-        //client.Set($"{parentPath}/{ActiveUser.Login}", ActiveUser);
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var wasUser = client.Get($"{parentPath}/{"fifa"}");
-            MyUser getUser = wasUser.ResultAs<MyUser>();
-            Console.WriteLine(getUser);
+            
         }
     }
 }

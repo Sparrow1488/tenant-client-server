92d4200087661964de9837bcc6c3c1200ff9bb9d
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-24 15:48:06 +0300

    test[Client-Test]: протестировал взаимодействие клиента и сервера: пока успешно

--- Тесты/Client-Test/Client-Test/Program.cs
+++ Тесты/Client-Test/Client-Test/Program.cs
@@ -3,24 +3,46 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace Client_Test
 {
     class Program
     {
+        //НА КЛИЕНТЕ НЕЛЬЗЯ ОДНОВРЕМЕННО ИСПОЛЬЗОВАТЬ И TcpClient и TcpListener
         static TcpClient client = new TcpClient();
-        static IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        //static TcpListener clientListener = new TcpListener(serverEndPoint);
+        static IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8090);
         static async Task Main(string[] args)
         {
-            Console.WriteLine("App start");
+            Console.ReadKey();
+            Console.WriteLine("Client start");
             client.Connect(serverEndPoint);
             NetworkStream stream = client.GetStream();
 
-            string request = "Client is connected!";
+            string request = "Hello world!!!!!!";
             byte[] data = Encoding.UTF8.GetBytes(request);
             await stream.WriteAsync(data, 0, data.Length);
             Console.WriteLine("Write in " +  stream.WriteTimeout);
 
+            Console.WriteLine("Wait for answer....");
+            var getStream = client.GetStream();
+            if (getStream.CanRead)
+            {
+                StringBuilder response = new StringBuilder();
+                byte[] getData = new byte[1024];
+                do
+                {
+                    int bytes = getStream.Read(getData, 0, getData.Length);
+                    response.Append(Encoding.UTF8.GetString(getData, 0, bytes));
+                }
+                while (getStream.DataAvailable);
+                Console.WriteLine("Get answer: " + response);
+            }
+            else
+            {
+                Console.WriteLine("Stream can not read");
+            }
             stream.Close();
             client.Close();
             Console.WriteLine("До свзяи...");
--- Тесты/Client-Test/Server-Test/Program.cs
+++ Тесты/Client-Test/Server-Test/Program.cs
@@ -8,10 +8,11 @@ namespace Server_Test
 {
     public class Program
     {
-        static IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        static IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8090);
         static TcpListener server = new TcpListener(localEndPoint);
         static async Task Main(string[] args)
         {
+            Console.ReadKey();
             Console.WriteLine("App started");
 
             server.Start();
@@ -26,7 +27,34 @@ namespace Server_Test
 
                 NetworkStream stream = getClient.GetStream();
                 byte[] getData = new byte[1024];
+                StringBuilder response = new StringBuilder();
+
+                if (stream.CanRead)
+                {
+                    do
+                    {
+                        int bytes = stream.Read(getData, 0, getData.Length);
+                        response.Append(Encoding.UTF8.GetString(getData, 0, bytes));
+                    } 
+                    while (stream.DataAvailable);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Get data: " + response);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine("Get stream can not read");
+                }
+
+                byte[] sendData = Encoding.UTF8.GetBytes("Ответ сервера");
+                await stream.WriteAsync(sendData, 0, sendData.Length);
+                Console.WriteLine("Write data in " + stream.WriteTimeout);
+
+                stream.Close();
             }
+            server.Stop();
+            Console.WriteLine("Server stop");
         }
     }
 }

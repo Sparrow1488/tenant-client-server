110b793b59915ca9f466458119f7eecdeeaba99f
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-25 21:35:48 +0300

    обновил

---
+++ Тесты/Client-Test/Server-Test/Classes/ServerTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server_Test.Classes
+{
+    public abstract class ServerTemplate
+    {
+        //TODO: реализовать базовый абстрактный класс сервера (под различные порты → задачи)
+    }
+}

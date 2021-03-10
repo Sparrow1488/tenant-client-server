fc3941a9e2e7e51b2a02ac7e9a4a244bee9b6ff3
Sparrow1488 <ilyaokubev@gmail.com>
2021-03-10 20:33:02 +0300

    work[Client]: добавлена возможность добавлять картинки на сервер

--- Main app/Tenant-server-app/WpfApp1/Pages/HomePages/LetterPage.xaml.cs
+++ Main app/Tenant-server-app/WpfApp1/Pages/HomePages/LetterPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using WpfApp1.MyApplication;
 using WpfApp1.Pages.HomePages.ChildLetterPage;
+using WpfApp1.Server.Packages.SourceDir;
 using WpfApp1.Server.ServerMeta;
 
 namespace WpfApp1.Pages.HomePages

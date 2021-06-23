0e6b855c6a34664fc0347a7ba98501aa3ab9c08c
Sparrow1488 <ilyaokubev@gmail.com>
2021-06-23 22:07:10 +0300

    start work[Client]: начал делать страницу для просмотра свих писем

---
+++ Main app/TenantClient/TenantClient/ViewModels/MyLettersVm.cs
@@ -0,0 +1,28 @@
+﻿using ExchangeSystem.v2.Entities;
+using System.Collections.ObjectModel;
+using TenantClient.Commands;
+
+namespace TenantClient.ViewModels
+{
+    internal class MyLettersVm : BaseVM
+    {
+        public ObservableCollection<Letter> MyLetters
+        {
+            get => _myLetters;
+            set
+            {
+                _myLetters = value;
+                OnPropertyChanged("MyLetters");
+            }
+        }
+        private ObservableCollection<Letter> _myLetters = new ObservableCollection<Letter>();
+
+        public MyCommand GetMyLetters
+        {
+            get => new MyCommand(async (obj) =>
+            {
+
+            });
+        }
+    }
+}

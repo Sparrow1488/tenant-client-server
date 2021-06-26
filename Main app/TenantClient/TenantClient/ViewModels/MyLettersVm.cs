df5d9f86cff2ffbd006fb8b7b923bd6578d7a206
Sparrow1488 <ilyaokubev@gmail.com>
2021-06-26 18:10:33 +0300

    work[Client]: добавлена возможность получать письма

--- Main app/TenantClient/TenantClient/ViewModels/MyLettersVm.cs
+++ Main app/TenantClient/TenantClient/ViewModels/MyLettersVm.cs
@@ -1,6 +1,10 @@
 ﻿using ExchangeSystem.v2.Entities;
+using ExchangeSystem.v2.Packages.Default;
+using ExchangeSystem.v2.Sendlers;
+using Newtonsoft.Json.Linq;
 using System.Collections.ObjectModel;
 using TenantClient.Commands;
+using TenantClient.Local;
 
 namespace TenantClient.ViewModels
 {
@@ -16,12 +20,24 @@ namespace TenantClient.ViewModels
             }
         }
         private ObservableCollection<Letter> _myLetters = new ObservableCollection<Letter>();
+        private string _authToken;
 
         public MyCommand GetMyLetters
         {
             get => new MyCommand(async (obj) =>
             {
-
+                var tokenWasExist = ClientTokenStorage.TryGet(out _authToken);
+                if (tokenWasExist)
+                {
+                    var request = new GetMyLetters();
+                    request.SetToken(_authToken);
+                    var sendler = new RequestSendler(new ConnectionSettings("127.0.0.1", 80));
+                    var response = await sendler.SendRequest(request);
+                    var jLetters = response.ResponseData as JArray;
+                    var letters = jLetters.ToObject<Letter[]>();
+                    foreach (var letter in letters)
+                        MyLetters.Add(letter);
+                }
             });
         }
     }
